a814eafbb14af99741e295aa742762c9982e0c22
Kristoffer Nolgren <kirstoffer@nolgren.se>
2014-07-16 16:25:10 +0200

    .

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <h1 class="mainHeader">Modernt Företagande</h1>
+        <div class="info">
+            <p>Allt du behöver för att komma igång med ditt företagande samlat på ett ställe. Med moderna webbbaserade tjänster kan du snabbt och enkelt spara både tid och pengar och öka chanserna att lyckas med ditt företag.</p>
+
+            <p>Läs mer om tjänsterna nedan och gå till respektive sajt för att komma igång.</p>
+        </div>
+        <div class="companies clearfix">
+            <section>
+                <p class="pre">lättare bokföring och fakturering</p>
+                <h1 class="companyheader">Zenconomy</h1>
+                <a class="action" href="https://www.zenconomy.se/priser/">Prova gratis</a>
+                <img class="printscreen" src="img/zenconomy.jpg" />
+                <p class="description">Minutemailer är en komplett lösning för att jobba med e-postmarknadsföring. Perfekt för småföretagare och designat för att hjälpa dig skapa bättre mejl som blir mer uppskattade av dina kunder och därför mer lästa.</p>
+                <a class="readmore" href="https://www.zenconomy.se/">Läs mer</a>
+            </section>
+            <section>
+                <p class="pre">skicka nyhetsbrev som blir lästa</p>
+                <h1 class="companyheader">Minutemailer</h1>
+                <a class="action" href="https://www.zenconomy.se/priser/">Prova gratis</a>
+                <img class="printscreen" />
+                <p class="description">Minutemailer är en komplett lösning för att jobba med e-postmarknadsföring. Perfekt för småföretagare och designat för att hjälpa dig skapa bättre mejl som blir mer uppskattade av dina kunder och därför mer lästa.</p>
+                <a class="readmore" href="https://www.zenconomy.se/">Läs mer</a>
+            </section>
+            <section>
+                <p class="pre">Starta företag utan företag</p>
+                <h1 class="companyheader">Firmify</h1>
+                <a class="action" href="https://www.zenconomy.se/priser/">Prova gratis</a>
+                <img class="printscreen" />
+                <p class="description">Minutemailer är en komplett lösning för att jobba med e-postmarknadsföring. Perfekt för småföretagare och designat för att hjälpa dig skapa bättre mejl som blir mer uppskattade av dina kunder och därför mer lästa.</p>
+                <a class="readmore" href="https://www.zenconomy.se/">Läs mer</a>
+            </section>
+            <section>
+                <p class="pre">Sälj dina fakturor till högstbjudande</p>
+                <h1 class="companyheader">Fakturabörsen</h1>
+                <a class="action" href="https://www.zenconomy.se/priser/">Prova gratis</a>
+                <img class="printscreen" />
+                <p class="description">Minutemailer är en komplett lösning för att jobba med e-postmarknadsföring. Perfekt för småföretagare och designat för att hjälpa dig skapa bättre mejl som blir mer uppskattade av dina kunder och därför mer lästa.</p>
+                <a class="readmore" href="https://www.zenconomy.se/">Läs mer</a>
+            </section>
+        </div>
+        <div class="info">
+            <h3>Om Modernt företagande</h3>
+            <p>Modernt företagande är grundat av en grupp av Svenska digitala tjänster och syftet är att du som företagare enkelt och lättöverskådligt ska kunna hitta de tjänster som kan hjälpa dig i din verksamhet.</p>
+            <p>Vill du vara med och forma Modernt företagande? <a href="mailto:kristoffer@zenconomy.se">Hör av dig.</a></p>
+        </div>
+
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
